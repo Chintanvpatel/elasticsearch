3b3bc47cf4f5f5c7044f5c09ed981f8f6c2cc12b
Chintan Patel <chintan.patel@qdata.io>
2016-07-18 19:23:04 +0530

    Docker entrypoint changes

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -17,6 +17,10 @@ if [ "$1" = 'elasticsearch' -a "$(id -u)" = '0' ]; then
 	#exec gosu elasticsearch "$BASH_SOURCE" "$@"
 fi
 
+# ECS will report the docker interface without help, so we override that with host's private ip
+AWS_PRIVATE_IP=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
+set -- "$@" --network.publish_host=$AWS_PRIVATE_IP
+
 # As argument is not related to elasticsearch,
 # then assume that user wants to run his own process,
 # for example a `bash` shell to explore this image
